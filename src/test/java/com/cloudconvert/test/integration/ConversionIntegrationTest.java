331d92c738773d9b8d10e76c66d1185c8bb51267
Paul Wellner Bou <paul@wellnerbou.de>
2021-04-06 17:46:02 +0200

    Add unit test testing conversion for images and for documents. The tests are green, while the real conversion fails in CloudConvert's backend

---
+++ src/test/java/com/cloudconvert/test/integration/ConversionIntegrationTest.java
@@ -0,0 +1,107 @@
+package com.cloudconvert.test.integration;
+
+import com.cloudconvert.client.CloudConvertClient;
+import com.cloudconvert.dto.Operation;
+import com.cloudconvert.dto.request.ConvertFilesTaskRequest;
+import com.cloudconvert.dto.request.UploadImportRequest;
+import com.cloudconvert.dto.response.OperationResponse;
+import com.cloudconvert.dto.response.Pageable;
+import com.cloudconvert.dto.response.TaskResponse;
+import com.cloudconvert.dto.result.Result;
+import com.cloudconvert.test.framework.AbstractTest;
+import com.cloudconvert.test.framework.IntegrationTest;
+import org.apache.http.HttpStatus;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@Category(IntegrationTest.class)
+@RunWith(JUnit4.class)
+public class ConversionIntegrationTest extends AbstractTest {
+
+    private CloudConvertClient cloudConvertClient;
+
+    @Before
+    public void before() throws Exception {
+        cloudConvertClient = new CloudConvertClient();
+    }
+
+    /**
+     * This test is green, but the conversion fails nevertheless
+     */
+    @Test
+    public void testCloudConvertDocumentConversion() throws IOException, URISyntaxException {
+        // Import upload (immediate upload)
+        final Result<TaskResponse> uploadImportTaskResponseResult = cloudConvertClient.importUsing().upload(
+                new UploadImportRequest(),
+                this.getClass().getResourceAsStream("/blank.docx")
+        );
+        assertThat(uploadImportTaskResponseResult.getStatus()).isEqualTo(HttpStatus.SC_OK);
+
+        final TaskResponse uploadImportTaskResponse = uploadImportTaskResponseResult.getBody();
+        assertThat(uploadImportTaskResponse.getOperation()).isEqualTo(Operation.IMPORT_UPLOAD);
+
+        // Convert
+        final ConvertFilesTaskRequest convertFilesTaskRequest = new ConvertFilesTaskRequest().setInput(
+                uploadImportTaskResponse.getId()
+        ).setInputFormat("docx").setOutputFormat("pdf");
+        final Result<TaskResponse> convertTaskResponseResult = cloudConvertClient.tasks().convert(convertFilesTaskRequest);
+        assertThat(convertTaskResponseResult.getStatus()).isEqualTo(HttpStatus.SC_CREATED);
+
+        final TaskResponse convertTaskResponse = convertTaskResponseResult.getBody();
+        assertThat(convertTaskResponse.getOperation()).isEqualTo(Operation.CONVERT);
+
+        // Wait
+        final Result<TaskResponse> waitConvertTaskResponseResult = cloudConvertClient.tasks().wait(convertTaskResponse.getId());
+        assertThat(waitConvertTaskResponseResult.getStatus()).isEqualTo(HttpStatus.SC_OK);
+    }
+
+    /**
+     * This test is just copied from {@link TasksIntegrationTest}.
+     */
+    @Test
+    public void convertFileTaskLifecycle() throws Exception {
+        // Import upload (immediate upload)
+        final Result<TaskResponse> uploadImportTaskResponseResult = cloudConvertClient.importUsing().upload(
+                new UploadImportRequest(),
+                this.getClass().getResourceAsStream("/image-test-file-1.jpg")
+        );
+        assertThat(uploadImportTaskResponseResult.getStatus()).isEqualTo(HttpStatus.SC_OK);
+
+        final TaskResponse uploadImportTaskResponse = uploadImportTaskResponseResult.getBody();
+        assertThat(uploadImportTaskResponse.getOperation()).isEqualTo(Operation.IMPORT_UPLOAD);
+
+        // List convert formats
+        final Result<Pageable<OperationResponse>> convertFormatsOperationResponsePageableResult = cloudConvertClient.tasks().convertFormats();
+        assertThat(convertFormatsOperationResponsePageableResult.getStatus()).isEqualTo(HttpStatus.SC_OK);
+
+        final List<OperationResponse> operationResponses = convertFormatsOperationResponsePageableResult.getBody().getData();
+        assertThat(operationResponses).anySatisfy(operationResponse -> assertThat(operationResponse.getOutputFormat()).isEqualTo("jpg"));
+
+        // Convert
+        final ConvertFilesTaskRequest convertFilesTaskRequest = new ConvertFilesTaskRequest().setInput(uploadImportTaskResponse.getId()).setInputFormat("jpg").setOutputFormat("png");
+        final Result<TaskResponse> convertTaskResponseResult = cloudConvertClient.tasks().convert(convertFilesTaskRequest);
+        assertThat(convertTaskResponseResult.getStatus()).isEqualTo(HttpStatus.SC_CREATED);
+
+        final TaskResponse convertTaskResponse = convertTaskResponseResult.getBody();
+        assertThat(convertTaskResponse.getOperation()).isEqualTo(Operation.CONVERT);
+
+        // Wait
+        final Result<TaskResponse> waitConvertTaskResponseResult = cloudConvertClient.tasks().wait(convertTaskResponse.getId());
+        assertThat(waitConvertTaskResponseResult.getStatus()).isEqualTo(HttpStatus.SC_OK);
+    }
+
+    @After
+    public void after() throws Exception {
+        cloudConvertClient.close();
+    }
+}

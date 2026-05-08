5f18251d429e09e59dd9a225676d009f33ac3e21
Nick Howes <n.howes@warwick.ac.uk>
2026-05-08 14:21:52 +0100

    Upgrade Jackson.
    Fixes: #61

--- src/main/java/com/cloudconvert/client/mapper/ObjectMapperProvider.java
+++ src/main/java/com/cloudconvert/client/mapper/ObjectMapperProvider.java
@@ -3,14 +3,14 @@ package com.cloudconvert.client.mapper;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.PropertyNamingStrategy;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 
 public class ObjectMapperProvider {
 
     public ObjectMapper provide() {
         return new ObjectMapper()
             .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
-            .setPropertyNamingStrategy(PropertyNamingStrategy.SNAKE_CASE)
-            .setSerializationInclusion(JsonInclude.Include.NON_NULL);
+            .setPropertyNamingStrategy(PropertyNamingStrategies.SNAKE_CASE)
+            .setDefaultPropertyInclusion(JsonInclude.Include.NON_NULL);
     }
 }
